f1c2d219b725072f28c630993a48ee50bcfc9e0f
Shammo Saha <168102614+Shammo1@users.noreply.github.com>
2025-10-13 21:48:32 +0600

    Add files via upload

---
+++ Codforces-pupilCoder/LearningPart-3/TwoPointer.cpp
@@ -0,0 +1,65 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define fi first
+#define se second
+#define fastio                    \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define sz(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define No cout << "NO" << '\n';
+#define Yes cout << "YES" << '\n';
+#define no cout << "No" << '\n';
+#define yes cout << "Yes" << '\n';
+#define in(vec, n)              \
+    for (int i = 0; i < n; i++) \
+        cin >> vec[i];
+#define out(vec, n)             \
+    for (int i = 0; i < n; i++) \
+        cout << vec[i] << " ";
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+///*    /\_/\
+// *   (= ._.)
+// *   / >  \>
+// */       //
+void solve()
+{
+    string s;
+    int n;
+    cin >> n >> s;
+    int cnt = 0;
+    int i, j, ans;
+    i = 0, j = n-1, ans = n;
+
+    while (i <= j)
+    {
+        if (s[i] == s[j])
+        {
+            break;
+        }
+        else
+        {
+            ans -= 2;
+            i++;
+            j--;
+        }
+    }
+    cout << ans<< endl;
+}
+int main()
+{
+    fastio int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
